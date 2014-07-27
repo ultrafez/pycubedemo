a41b67ae001b3e452163002db6588fef4eac5cc4
Alex Silcock <Alexander.Silcock@bskyb.com>
2014-07-27 17:02:02 +0100

    not really working at all

--- patterns/audioblob.py
+++ patterns/audioblob.py
@@ -6,74 +6,56 @@ import cubehelper
 import pyaudio
 import struct
 import math
+import audioop
 
 FORMAT = pyaudio.paInt24
 SHORT_NORMALIZE = (1.0/32768.0)
-CHANNELS = 2
-RATE = 44100  
+CHANNELS = 1
+RATE = 44100
 INPUT_BLOCK_TIME = 0.05
-INPUT_FRAMES_PER_BLOCK = int(RATE*INPUT_BLOCK_TIME)
+# INPUT_FRAMES_PER_BLOCK = int(RATE*INPUT_BLOCK_TIME)
+INPUT_FRAMES_PER_BLOCK = 1024
 
 class Pattern(object):
     def init(self):
         self.double_buffer = True
+        self.current_audio = None
+        self.history = []
+        self.min_amplitude = 0
+        self.max_amplitude = 17000
+        self.amplitude_diff = 17000
 
         pa = pyaudio.PyAudio()
         self.stream = pa.open(format = FORMAT,
                       channels = CHANNELS,
                       rate = RATE,
                       input = True,
-                      frames_per_buffer = INPUT_FRAMES_PER_BLOCK)
+                      frames_per_buffer = INPUT_FRAMES_PER_BLOCK,
+                      stream_callback = self.incoming_audio)
 
-        return 1.0 / self.cube.size
+        return 0.5 / self.cube.size
+
+    def incoming_audio(self, in_data, frame_count, time_info, status_flags):
+        self.current_audio = in_data
+        return (None, pyaudio.paContinue)
 
     def tick(self):
-        try:
-            block = self.stream.read(INPUT_FRAMES_PER_BLOCK)
-        except IOError as e:
-            print "I/O error({0}): {1}".format(e.errno, e.strerror)
-            print "Error recording in audioblob"
-            raise StopIteration
+        if self.current_audio:
+            amplitude = audioop.rms(self.current_audio, 2)
+            print amplitude
+            self.history.append(amplitude)
+            size = max(0, (amplitude - self.min_amplitude) / float(self.amplitude_diff))
+            self.draw(math.floor(size * self.cube.size))
 
-        amplitude = self.get_rms(block)
-        print amplitude
-        return
+        if len(self.history) > 16:
+            # print "updating"
+            self.min_amplitude = min(self.history)
+            self.amplitude_diff = self.max_amplitude - self.min_amplitude
+            self.history = []
 
-        if amplitude > tap_threshold: # if its to loud...
-            quietcount = 0
-            noisycount += 1
-            if noisycount > OVERSENSITIVE:
-                tap_threshold *= 1.1 # turn down the sensitivity
+    def draw(self, size):
+        for coord in cubehelper.line((0, 0, 0), (7, 0, 0)):
+            self.cube.set_pixel(coord, (0, 0, 0))
 
-        else: # if its to quiet...
-
-            if 1 <= noisycount <= MAX_TAP_BLOCKS:
-                print 'tap!'
-            noisycount = 0
-            quietcount += 1
-            if quietcount > UNDERSENSITIVE:
-                tap_threshold *= 0.9 # turn up the sensitivity
-
-    def get_rms(self, block):
-        # Ripped from StackOverflow http://stackoverflow.com/a/10669054/172666
-
-        # RMS amplitude is defined as the square root of the 
-        # mean over time of the square of the amplitude.
-        # so we need to convert this string of bytes into 
-        # a string of 16-bit samples...
-
-        # we will get one short out for each 
-        # two chars in the string.
-        count = len(block) / 2
-        format = "%dh" % (count)
-        shorts = struct.unpack(format, block)
-
-        # iterate over the block.
-        sum_squares = 0.0
-        for sample in shorts:
-            # sample is a signed short in +/- 32768. 
-            # normalize it to 1.0
-            n = sample * SHORT_NORMALIZE
-            sum_squares += n*n
-
-        return math.sqrt(sum_squares / count)
+        for coord in cubehelper.line((0, 0, 0), (size, 0, 0)):
+            self.cube.set_pixel(coord, (1.0, 1.0, 1.0))
